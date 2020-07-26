7ea6d5de49e39cc886656595e79a30d406a64d66
Tai Bui <vantaibui92@gmail.com>
2020-07-26 22:39:47 +0700

    Updates

---
+++ precache-manifest.a0369f2bde2c2e33b15424f84d0801d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733a78e5ae3a4194afbe1a2b0e7556c0",
+    "url": "/BookingMovie/index.html"
+  },
+  {
+    "revision": "3e5c5bb93c7771990b3e",
+    "url": "/BookingMovie/static/css/main.baf1edbe.chunk.css"
+  },
+  {
+    "revision": "c0f6a963237c51db892f",
+    "url": "/BookingMovie/static/js/2.11271035.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BookingMovie/static/js/2.11271035.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5c5bb93c7771990b3e",
+    "url": "/BookingMovie/static/js/main.f9bc1428.chunk.js"
+  },
+  {
+    "revision": "e7588b8e6ef2886bb796",
+    "url": "/BookingMovie/static/js/runtime-main.ee18d6a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BookingMovie/static/media/logo.5d5d9eef.svg"
+  }
+]);
